f4082fb534f2bfcc5cc879131fa86350c48f4bac
JPMESRocks <joaopaulo1236@gmail.com>
2022-10-03 14:15:39 +0200

    final fixes

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from imp import init_builtin
 import numpy as np
 import matplotlib.pyplot as plt
 print('I am a golf ball and I want to fly.')
@@ -11,7 +10,20 @@ g=9.81
 rho=1.2
 A= np.pi*r**2
 
-def golfball(theta: float =60, drag: bool = True, lift: bool = True):
+def golfball(initialangle: float =60, drag: bool = True, lift: bool = True):
+    """
+
+    Numerically calculates the trajectory of a golfball being projected at some angle, taking into account
+    drag and lift. Plots the x-y trajectory and graphs the x and y components of acceleration throughout its flight.
+
+    Arguments:
+                initialangle    (float)         The angle in degrees at which the ball is "thrown"
+                drag            (boolean)       Whether the drag force is calculated and taken into account
+                lift            (boolean)       Whether the lift force is calculated and taken into account
+    Returns:
+            None
+
+    """
     # setting drag/lift
     c_d=0.3 if drag else 0
     c_l=0.3 if lift else 0
@@ -29,9 +41,9 @@ def golfball(theta: float =60, drag: bool = True, lift: bool = True):
     t_space = np.linspace(0,max_t,ticks)
 
     # initial conditions. (change init angle here)
-    conv_theta=(np.pi/180)*theta
+    conv_angle=(np.pi/180)*initialangle
     init_v=60
-    v[0,0]=init_v*np.cos(conv_theta); v[1,0]=init_v*np.sin(conv_theta)
+    v[0,0]=init_v*np.cos(conv_angle); v[1,0]=init_v*np.sin(conv_angle)
     a[1,0]=-g
 
 
@@ -65,6 +77,8 @@ def golfball(theta: float =60, drag: bool = True, lift: bool = True):
     ax1.axis('scaled')
     ax1.set_ylabel("y (m)")
     ax1.set_xlabel("x (m)")
+    ax1.set_ylim(-10,200)
+    ax1.set_xlim(-10,350)
 
     ax2.plot(t_space, a[0])
     ax2.set_ylabel(r"$a_x\;(ms^{-2})$")
@@ -74,7 +88,7 @@ def golfball(theta: float =60, drag: bool = True, lift: bool = True):
     ax3.set_ylabel(r"$a_y\;(ms^{-2})$")
     ax3.set_xlabel("t (s)")
 
-    fig.suptitle(f"initial angle: {theta}, drag: {drag}, lift: {lift}")
+    fig.suptitle(f"initial angle: {initialangle}, drag: {drag}, lift: {lift}")
 
     print(f"Flight time: {round(t_space[i],2)}s, Distance travelled: {round(r[0,i],2)}m")
     plt.show()
@@ -82,22 +96,22 @@ def golfball(theta: float =60, drag: bool = True, lift: bool = True):
 
 # In each component of acceleration there is a jump in the first time step because of 
 # the initial condition being different(a_x=0, a_y=-g). This is just an artifact of our numerical method. 
-golfball(theta=60,drag=True,lift=True)
+golfball(initialangle=60,drag=True,lift=True)
 
 # Interestingly, if we only disable drag. The non-linearity of the y-acceleration becomes more visible
-golfball(theta=60, drag=False, lift=True)
+golfball(initialangle=60, drag=False, lift=True)
 
-# For the ball being shot straight up (theta=90), the trajectory is correct and makes sense. However, the acceleration graphs behave very weirdly, forming a solid triangle.
+# For the ball being shot straight up (initialangle=90), the trajectory is correct and makes sense. However, the acceleration graphs behave very weirdly, forming a solid triangle.
 # When looking at the values of the acceleration themselves, the drag+lift seem to oscillate between positive and negative values in each acceleration component.
 
 # I believe this occurs because these forces depend on the ball's velocity, who's x-component is initially zero. In the first step, lift pushes the ball into a negative x-velocity,
 # As soon as this occurs, that negative x-velocity causes a stronger drag force pushing it in the +x direction bringing it back to around 90 degrees. This occurs over and over
 # until eventually the lift dominates at 6s and the ball falls around x=-1.7m.
-golfball(theta=90,drag=True,lift=True)
+golfball(initialangle=90,drag=True,lift=True)
 
 # Same thing, different angle. Everything lookin good.
-golfball(theta=20,drag=True,lift=True)
+golfball(initialangle=20,drag=True,lift=True)
 
 # shooting the golfball parallel to the ground at the ground provides the expected result considering our loop's stop statement. The loop iterates once
 # until it realizes the ball is on the ground and stops.
-golfball(theta=0,drag=True,lift=True)
+golfball(initialangle=0,drag=True,lift=True)
